7021e64f4819bcdb9a90700ae8efb712c22e3ac8
BreyMene <menesesmunozfelipe@gmail.com>
2024-10-02 19:38:18 -0500

    Add files via upload
    Librería de Entidades

---
+++ lib_entidades/Modelos/Barrios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lib_entidades.Modelos
+{
+    public class Barrios
+    {
+        [Key] public int Id { get; set; } //Id de Barrio
+        public string? Nombre { get; set; } //Nombre del Barrio
+        public int Ciudad { get; set; } //ID de la ciudad a la que pertenece
+
+        [NotMapped] public Ciudades? _Ciudad { get; set; } // Instancia a ciudad, no mapeada a la base de datos.
+
+    }
+}
---
+++ lib_entidades/Modelos/Ciudades.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lib_entidades.Modelos
+{
+    public class Ciudades
+    {
+        [Key] public int Id { get; set; } //Id de la Ciudad (Llave)
+        public string? Nombre { get; set; }// Nombre de la Ciudad
+        public int Departamento { get; set; }// Id del departamento al que pertenece 
+
+        [NotMapped] public Departamentos? _Departamento { get; set; } // Instancia a Departamentos, no mapeada a la base de datos.
+
+    }
+}
