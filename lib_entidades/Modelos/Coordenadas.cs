39fd1a5e829b29f529b8d65444046961fc2678b1
BreyMene <menesesmunozfelipe@gmail.com>
2024-10-02 19:39:41 -0500

    Libreria_Entidades

---
+++ lib_entidades/Modelos/Coordenadas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lib_entidades.Modelos
+{
+    public class Coordenadas
+    {
+        [Key] public int Id { get; set; } // Id de la coordenada (Llave)
+        public string? Latitud { get; set; } // Latitud de la coordenada (de norte a sur)
+        public string? Longitud { get; set; }//Longitud horizontal (De este a oeste)
+
+    }
+}
+
---
+++ lib_entidades/Modelos/Paises.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lib_entidades.Modelos
+{
+    public class Paises
+    {
+        [Key] public int Id { get; set; } //Id del Pais (Llave)
+        public string? Nombre { get; set; } // Nombre del Pais
+
+    }
+}
+
+
